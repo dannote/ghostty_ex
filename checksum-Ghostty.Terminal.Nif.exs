5e3c9a26fedfde700fa41c8f20b9546829a9cedf
Danila Poyarkov <dev@dannote.net>
2026-03-30 14:49:48 +0300

    Update precompiled NIF checksums for v0.1.0

--- checksum-Ghostty.Terminal.Nif.exs
+++ checksum-Ghostty.Terminal.Nif.exs
@@ -1,1 +1,8 @@
-[]
+[
+  {"Elixir.Ghostty.Terminal.Nif-v0.1.0-aarch64-linux-gnu.so.tar.gz",
+   "sha256:ec0acbde42c65fda0f0e8477e3946cd2ff899230359b373c0e73324c623921bf"},
+  {"Elixir.Ghostty.Terminal.Nif-v0.1.0-aarch64-macos-none.so.tar.gz",
+   "sha256:e82410d96519262e56aa46d16bbde1825cfc5920477b982db8dbb3d32e9b4f54"},
+  {"Elixir.Ghostty.Terminal.Nif-v0.1.0-x86_64-linux-gnu.so.tar.gz",
+   "sha256:6b182a99fb0ed4c777191683e3ccaa1c6dc76a191eb1cbb2e183ebd1d7b1c369"}
+]
